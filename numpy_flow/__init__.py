73bd8b80adac3b6c9b512ded01941a9e62604ba7
zhangdongkun98 <zhangdongkun98@gmail.com>
2021-12-20 16:02:38 +0800

    first commit

---
+++ numpy_flow/__init__.py
@@ -0,0 +1,1 @@
+print('This is numpy_flow.')
---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='numpy_flow',
+    packages=find_packages(),
+    version='0.0.1',
+    author='Zhang Dongkun',
+    author_email='zhangdongkun98@gmail.com',
+    url='https://github.com/zhangdongkun98/numpy-flow',
+    description='Nothing at all.',
+    install_requires=[
+    ],
+
+    include_package_data=True
+)
